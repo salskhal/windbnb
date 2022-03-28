aa0ab179681b4bd8589e6334de6d028465a187a1
salskhal <olaniyisal@gmail.com>
2022-03-29 00:06:46 +0100

    Features/ little responsive

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
   <nav>
-    <img src="./assets/logo.svg" alt="logo">
+    <img src="./assets/logo.svg" alt="logo" />
     <div class="filter--search">
-      <input type="text" placeholder="Input City" v-model="search">
+      <input type="text" placeholder="Input City" v-model="search" />
     </div>
   </nav>
   <div class="container">
     <div class="header">
       <h2>Stays in Finland</h2>
-      <p>{{mathcingCities.length}} stays</p>
+      <p>{{ mathcingCities.length }} stays</p>
     </div>
     <!-- stays grid -->
-    <div class="stays-grid" >
+    <div class="stays-grid">
       <!-- each stay element -->
       <div class="stay-card" v-for="stay in mathcingCities" :key="stay.title">
-        <img :src="stay.photo" alt="">
+        <img :src="stay.photo" alt="" />
         <div class="descriptions">
           <div class="type-rating">
-            <p>{{stay.type}}</p>
-            <p class="rating">⭐ {{stay.rating}}</p>
+            <p>{{ stay.type }}</p>
+            <p class="rating">⭐ {{ stay.rating }}</p>
           </div>
-          <h5>{{stay.title}}</h5>
-          <p>{{stay.city}}</p>
+          <h5>{{ stay.title }}</h5>
+          <p>{{ stay.city }}</p>
         </div>
       </div>
     </div>
@@ -29,105 +29,125 @@
 </template>
 
 <script>
-
 import data from "./data/stays.json";
-
 
 export default {
   name: "App",
-  data(){
-    return{
+  data() {
+    return {
       stays: data,
-      search: ""
-    }
-  }, 
+      search: "",
+    };
+  },
   computed: {
-     mathcingCities() {
+    mathcingCities() {
       return this.stays.filter((stay) => stay.city.includes(this.search));
     },
-  }
+  },
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Mulish&display=swap');
-*{
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Mulish&display=swap");
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 
-body{
+body {
   background: rgb(247, 243, 243);
 }
 
-nav{
+nav {
   padding: 20px 60px;
   display: flex;
   justify-content: space-between;
-  
 }
-.filter--search{
+.filter--search {
   padding: 10px;
   border-radius: 10px;
-  box-shadow: 0px 1px 6px 0px #0000001A;
+  box-shadow: 0px 1px 6px 0px #0000001a;
   font-family: "Mulish";
 }
-.filter--search input{
+.filter--search input {
   border: none;
   outline: none;
   background: none;
 }
-.filter--search input:active{
+.filter--search input:active {
   outline: none;
 }
-.filter--search input::placeholder{
+.filter--search input::placeholder {
   font-family: "Mulish";
 }
 
-.container{
+.container {
   font-family: "Montserrat", sans-serif;
   padding-inline: 60px;
   margin-top: 30px;
 }
 
-.header{
+.header {
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
 
-
-.stays-grid{
+.stays-grid {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 20px;
   margin-top: 20px;
 }
 
-.stays-grid img{
+.stays-grid img {
   width: 100%;
   height: 300px;
   border-radius: 24px;
 }
 
+@media screen and (max-width: 900px) {
+  .stays-grid {
+    grid-template-columns: repeat(2, 1fr);
+  }
 
-.stay-card{
-  transition: .25s all ease-in-out;
+  .stays-grid img{
+    height: 200px;
+  }
 }
 
-.stay-card:hover{
-   box-shadow: 0px 1px 6px 0px #0000001A;
-   border-radius: 24px;
-   overflow: hidden;
+
+@media screen and (max-width: 600px){
+  nav{
+    padding-inline: 20px;
+  }
+  .container{
+    padding-inline: 20px;
+  }
+ .stays-grid {
+    grid-template-columns: 1fr;
+  }
 }
 
-.descriptions{
+
+
+.stay-card {
+  transition: 0.25s all ease-in-out;
+}
+
+.stay-card:hover {
+  box-shadow: 0px 1px 6px 0px #0000001a;
+  border-radius: 24px;
+  overflow: hidden;
+}
+
+.descriptions {
   padding: 20px;
 }
 
-.type-rating{
+.type-rating {
   display: flex;
   justify-content: space-between;
   margin-bottom: 20px;
